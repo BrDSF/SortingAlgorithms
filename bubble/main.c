4493bf673e8586be35edf4500da95025a893f547
Brayan Freitas <brayandeyvid17@gmail.com>
2022-03-09 14:26:23 -0300

    improving bubble

--- bubble/main.c
+++ bubble/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 void bubbleSort(int *, int);
+void swap(int *, int *);
 
 int main(void)
 {
@@ -22,7 +24,7 @@ int main(void)
   bubbleSort(p, size);
   for (int i = 0; i < size; i++)
   {
-    printf("Ordenated : %d\n", p[i]);
+    printf("Ordenated, position(%d) : %d\n", i + 1, p[i]);
   }
 
   return 0;
@@ -30,17 +32,44 @@ int main(void)
 
 void bubbleSort(int *vet, int vetSize)
 {
-  char aux;
-  for (int i = 1; i < vetSize - 1; i++)
+  int last = vetSize;
+  int position = vetSize;
+  bool change = true;
+  while (change)
   {
-    for (int j = vetSize - 1; j >= i; j--)
+    change = false;
+    for (int i = 0; i < last; i++)
     {
-      if (vet[j - 1] > vet[j])
+      if (vet[i] > vet[i + 1])
       {
-        aux = vet[j - 1];
-        vet[j - 1] = vet[j];
-        vet[j] = aux;
+        swap(&vet[i], &vet[i + 1]);
+        change = true;
+        position = i;
       }
     }
+    last = position;
   }
 }
+
+void swap(int *p1, int *p2)
+{
+  int aux = *p1;
+  *p1 = *p2;
+  *p2 = aux;
+}
+// void bubbleSort(int *vet, int vetSize)
+// {
+//   char aux;
+//   for (int i = 1; i < vetSize - 1; i++)
+//   {
+//     for (int j = vetSize - 1; j >= i; j--)
+//     {
+//       if (vet[j - 1] > vet[j])
+//       {
+//         aux = vet[j - 1];
+//         vet[j - 1] = vet[j];
+//         vet[j] = aux;
+//       }
+//     }
+//   }
+// }
---
+++ selection/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void selectionSort();
+
+int main(void)
+{
+  int size;
+
+  printf("Type the size of the array: ");
+  scanf("%d", &size);
+
+  int *p = (int *)malloc(size * sizeof(int));
+  srand(time(NULL));
+  for (int i = 0; i < size; i++)
+  {
+    p[i] = rand() % 100;
+  }
+
+  for (int i = 0; i < size; i++)
+  {
+    printf("Ordenated : %d\n", p[i]);
+  }
+
+  return 0;
+}
